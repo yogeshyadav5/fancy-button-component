8ff282fc067b82faafb428a613521cfeab743d00
Yogesh Yadav <yogesh.yadavyj@gmail.com>
2022-12-03 07:48:39 +0530

    reinitialized files

---
+++ src/components/Button/Button.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './Button.css';
+
+import { buttonTypes, buttonVariants, buttonSizes } from './constant';
+
+const Button = (props) => {
+  const {
+    text,
+    isDisabled,
+    type,
+    size,
+    backgroundColor,
+    textColor,
+    onClick,
+    shadow,
+  } = props;
+
+  const shadows = shadow ? 'shadow' : '';
+  const disabled = isDisabled ? 'disabled' : '';
+
+  return (
+    <button
+      onClick={onClick}
+      className={[
+        'button',
+        `${size}`,
+        `${type}`,
+        `${shadows}`,
+        `${disabled}`,
+        `bg-${backgroundColor}`,
+        `text-${textColor}`,
+      ].join(' ')}
+      disabled={isDisabled}
+      {...props}
+    >
+      
+        {text}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  text: PropTypes.string,
+  isDisabled: PropTypes.bool,
+  type: PropTypes.oneOf([
+    buttonTypes.outline,
+    buttonTypes.filled,
+    buttonTypes.text,
+  ]),
+  variant: PropTypes.oneOf([buttonVariants.oval, buttonVariants.rectangular]),
+  size: PropTypes.oneOf([
+    buttonSizes.small,
+    buttonSizes.medium,
+    buttonSizes.large,
+  ]),
+  backgroundColor: PropTypes.string,
+  textColor: PropTypes.string,
+  onClick: PropTypes.func,
+};
+
+Button.defaultProps = {
+  isDisabled: false,
+  type: 'filled',
+  variant: 'oval',
+  size: 'medium',
+  backgroundColor: 'transparent',
+  textColor: 'white',
+  shadow:true
+};
+
+
+
+export { Button };
---
+++ src/components/Button/Button.stories.js
@@ -0,0 +1,100 @@
+import React from 'react';
+
+import { Button } from './Button';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  title: 'React/Button',
+  argTypes: {
+    text: { control: 'text' },
+    isDisabled: { control: 'boolean' },
+    shadow: { control: 'boolean' },
+    size: {
+      control: { type: 'select', options: ['small', 'medium', 'large'] },
+    },
+    type: {
+      control: { type: 'select', options: ['filled', 'outline', 'text'] },
+    },
+    textColor: {
+      control: { type: 'select', options: ['default', 'white', 'primary'] },
+    },
+  },
+};
+
+const Template = (args) => <Button {...args} />;
+
+export const defaultButton = Template.bind({});
+defaultButton.args = {
+  type: 'default',
+  text: 'Default',
+  backgroundColor: 'default',
+  textColor: 'default',
+  shadow: true,
+};
+
+export const defaultDisabledButton = Template.bind({});
+defaultDisabledButton.args = {
+  text: 'Disabled',
+  backgroundColor: 'default',
+  textColor: 'default',
+  shadow: false,
+  isDisabled: true,
+};
+
+export const outlineButton = Template.bind({});
+outlineButton.args = {
+  text: 'Default',
+  textColor: 'primary',
+  type: 'outline',
+};
+
+export const textButton = Template.bind({});
+textButton.args = {
+  text: 'Default',
+  backgroundColor: 'transparent',
+  textColor: 'primary',
+  shadow: false,
+  type: 'text',
+};
+
+export const textDisabledButton = Template.bind({});
+textDisabledButton.args = {
+  text: 'Disabled',
+  shadow: false,
+  isDisabled: true,
+  textColor: 'default',
+  type: 'text',
+};
+
+export const disableShadow = Template.bind({});
+disableShadow.args = {
+  text: 'Default',
+  backgroundColor: 'primary',
+  type: 'filled',
+  textColor: 'white',
+  shadow: true,
+};
+
+
+export const primaryButton = Template.bind({});
+primaryButton.args = {
+  text: 'Default',
+  backgroundColor: 'primary',
+  textColor: 'white',
+  shadow: true,
+};
+export const secondaryButton = Template.bind({});
+secondaryButton.args = {
+  text: 'Secondary',
+  backgroundColor: 'secondary',
+  textColor: 'white',
+  shadow: true,
+};
+
+export const dangerButton = Template.bind({});
+dangerButton.args = {
+  text: 'Danger',
+  backgroundColor: 'danger',
+  textColor: 'white',
+  shadow: true,
+};
